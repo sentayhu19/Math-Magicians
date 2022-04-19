d94a03d39b28faf565d72497a13807969a38fad5
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-19 18:40:17 +0300

    public folder cleaned and class used on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (<Calculator />);
+  }
+}
 export default App;

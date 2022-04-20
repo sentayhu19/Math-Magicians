78865a4dd0a991319f6353ac9ee22a6974ba8fa6
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-20 17:13:53 +0300

    Unnecessary wrap removed

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,45 +19,43 @@ const Calculator = () => {
   };
 
   return (
-    <div className="main-wrap">
-      <div className="calc-wrap">
-        <div id="input">
-          {total}
-          {' '}
-          {operation}
-          {' '}
-          {next}
-        </div>
-        <div id="row1">
-          <button type="button" className="op" onClick={handleClick}>AC</button>
-          <button type="button" className="op" onClick={handleClick}>+/-</button>
-          <button type="button" className="op" onClick={handleClick}>%</button>
-          <button type="button" className="operator" onClick={handleClick}>÷</button>
-        </div>
-        <div id="row2">
-          <button type="button" className="number" id="7" onClick={handleClick}>7</button>
-          <button type="button" className="number" id="8" onClick={handleClick}>8</button>
-          <button type="button" className="number" id="9" onClick={handleClick}>9</button>
-          <button type="button" className="operator" onClick={handleClick}>*</button>
-        </div>
-        <div id="row3">
-          <button type="button" className="number" id="4" onClick={handleClick}>4</button>
-          <button type="button" className="number" id="5" onClick={handleClick}>5</button>
-          <button type="button" className="number" id="6" onClick={handleClick}>6</button>
-          <button type="button" className="operator" onClick={handleClick}>-</button>
-        </div>
+    <div className="calc-wrap">
+      <div id="input">
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+      </div>
+      <div id="row1">
+        <button type="button" className="op" onClick={handleClick}>AC</button>
+        <button type="button" className="op" onClick={handleClick}>+/-</button>
+        <button type="button" className="op" onClick={handleClick}>%</button>
+        <button type="button" className="operator" onClick={handleClick}>÷</button>
+      </div>
+      <div id="row2">
+        <button type="button" className="number" id="7" onClick={handleClick}>7</button>
+        <button type="button" className="number" id="8" onClick={handleClick}>8</button>
+        <button type="button" className="number" id="9" onClick={handleClick}>9</button>
+        <button type="button" className="operator" onClick={handleClick}>*</button>
+      </div>
+      <div id="row3">
+        <button type="button" className="number" id="4" onClick={handleClick}>4</button>
+        <button type="button" className="number" id="5" onClick={handleClick}>5</button>
+        <button type="button" className="number" id="6" onClick={handleClick}>6</button>
+        <button type="button" className="operator" onClick={handleClick}>-</button>
+      </div>
 
-        <div id="row4">
-          <button type="button" className="number" id="1" onClick={handleClick}>1</button>
-          <button type="button" className="number" id="2" onClick={handleClick}>2</button>
-          <button type="button" className="number" id="3" onClick={handleClick}>3</button>
-          <button type="button" className="operator" onClick={handleClick}>+</button>
-        </div>
-        <div id="row5">
-          <button type="button" id="n0" onClick={handleClick}>0</button>
-          <button type="button" className="number" onClick={handleClick}><b>.</b></button>
-          <button className="operator" type="button" onClick={handleClick}>=</button>
-        </div>
+      <div id="row4">
+        <button type="button" className="number" id="1" onClick={handleClick}>1</button>
+        <button type="button" className="number" id="2" onClick={handleClick}>2</button>
+        <button type="button" className="number" id="3" onClick={handleClick}>3</button>
+        <button type="button" className="operator" onClick={handleClick}>+</button>
+      </div>
+      <div id="row5">
+        <button type="button" id="n0" onClick={handleClick}>0</button>
+        <button type="button" className="number" onClick={handleClick}><b>.</b></button>
+        <button className="operator" type="button" onClick={handleClick}>=</button>
       </div>
     </div>
   );

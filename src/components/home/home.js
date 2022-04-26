dd23975bf13028f23deb6f719143d4f005c02be0
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-26 18:14:57 +0300

    Home and quote component created

---
+++ src/components/home/home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default home =()=>{
+return (
+<>
+<p>As time passed, many mathematicians gravitated towards universities. An emphasis on free thinking and experimentation had begun in Britain's oldest
+     universities beginning in the seventeenth century at Oxford with the scientists Robert Hooke and Robert Boyle, and at Cambridge where Isaac
+      Newton was Lucasian Professor of Mathematics and
+     Physics. Moving into the 19th century, the objective of universities all across Europe evolved from teaching the "regurgitation of knowledge" to 
+    "encourag[ing] productive thinking."[5] In 1810, Humboldt convinced the King of Prussia to build a university in Berlin based on Friedrich Schleiermacher's
+     liberal ideas; the goal was to demonstrate the process of the discovery of knowledge and to teach students to "take account of 
+     fundamental laws of science in all their thinking."</p>
+     
+<p>British universities of this period adopted some approaches familiar to the Italian and German universities, but as they already enjoyed
+     substantial freedoms and autonomy the changes there had begun with the Age of Enlightenment, the same influences that inspired Humboldt.
+      The Universities of Oxford and Cambridge emphasized the importance of research, arguably more authentically implementing Humboldt's idea 
+      of a university than even German universities, which were subject to state authority.[7] Overall, science (including mathematics) became 
+      the focus of universities in the 19th and 20th centuries. </p>
+</>
+
+)
+}
---
+++ src/components/quotes/quotes.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+let quote = () =>
+{
+    return (
+        <>
+        <p>mathematician is not about numbers, equations, computations,
+           or algorithms: it is about understanding -William paul Thurston
+        </p>
+        </>
+    )
+}
